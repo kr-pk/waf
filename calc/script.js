e285f437aa4550cc4623c6546515804bcbcc6c6f
Kr. PK <90453384+kr-pk@users.noreply.github.com>
2022-05-22 19:06:37 +0530

    Add files via upload

---
+++ calc/script.js
@@ -0,0 +1,19 @@
+let screen = document.getElementById("screen");
+buttons = document.querySelectorAll("button");
+let screenValue = "";
+for (item of buttons) {
+  item.addEventListener("click", (e) => {
+    buttonText = e.target.innerText;
+    console.log("Button Text Is ", buttonText);
+
+    if (buttonText == "AC") {
+      screenValue = "";
+      screen.value = screenValue;
+    } else if (buttonText == "=") {
+      screen.value = eval(screenValue);
+    } else {
+      screenValue += buttonText;
+      screen.value = screenValue;
+    }
+  });
+}
